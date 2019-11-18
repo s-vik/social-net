86334470e2f09c00857d697fba834a5af8c2f217
Viktor Sysyuk <s.vik.json@gmail.com>
2019-11-18 16:35:41 +0200

    an app refactoring, added store(redux basics)

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import { addPost, changeValuePost, addMessage, changeValueDialogsMessage, subscribe } from './redux/state';
+import store from './redux/store';
 
-export const renderEntireTree = (prop) => {
-    ReactDOM.render(<BrowserRouter><App
-        addMessage={addMessage}
-        changeValuePost={changeValuePost}
-        changeValueDialogsMessage={changeValueDialogsMessage}
-        addPost={addPost}
-        state={state || prop.state} />
+const renderEntireTree = (state) => {
+    ReactDOM.render(<BrowserRouter>
+        <App
+            state={state}
+            addMessage={store.addMessage.bind(store)}
+            changeValueDialogsMessage={store.changeValueDialogsMessage.bind(store)}
+            changeValuePost={store.changeValuePost.bind(store)}
+            addPost={store.addPost.bind(store)}
+        />
     </BrowserRouter>, document.getElementById('root'));
 
 }
-renderEntireTree();
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ src/redux/store.js
@@ -0,0 +1,70 @@
+const store = {
+    _subscriber() {
+        console.log('observer is undefined');
+    },
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Viktor', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+                { id: 2, name: 'Inna', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+                { id: 3, name: 'Dimych', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+                { id: 4, name: 'Ihor', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+                { id: 5, name: 'Toxa', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+                { id: 6, name: 'Ruslan', ava: 'https://download-cs.net/steam/avatars/3377.jpg' }
+            ],
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How is your React?' },
+                { id: 3, message: 'Thanks, very well' }
+            ],
+            inputValueDialogsPage: 'some text'
+        },
+        profilePage: {
+            posts: [
+                { id: 1, likeCount: 25, message: 'Hi, how are you?' },
+                { id: 2, likeCount: 30, message: 'I\'m fine, and you?' }
+            ],
+            inputValue: 'I\'m the best'
+        },
+        sidebar: {
+            friends: [{ name: 'Inna', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+            { name: 'Toxa', ava: 'https://download-cs.net/steam/avatars/3377.jpg' },
+            { name: 'Dima', ava: 'https://download-cs.net/steam/avatars/3377.jpg' }]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._subscriber = observer;
+    },
+    addPost(postText) {
+        let newPost = {
+            id: 5,
+            likeCount: 0,
+            message: postText
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.inputValue = '';
+        this._subscriber(this._state);
+    },
+    changeValuePost(currentText) {
+        this._state.profilePage.inputValue = currentText;
+        this._subscriber(this._state);
+    },
+    addMessage(textMessage) {
+        let newMessage = {
+            id: 3,
+            message: textMessage
+        }
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.inputValueDialogsPage = '';
+        this._subscriber(this._state);
+    },
+    changeValueDialogsMessage(currentText) {
+        this._state.dialogsPage.inputValueDialogsPage = currentText;
+        this._subscriber(this._state);
+    }
+}
+
+export default store;
